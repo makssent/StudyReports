80982f29696bf614bbb2d023dca93f494426f59b
vasilevskyy <marvo.rate2016@yandex.ru>
2024-03-16 20:25:15 +0300

    1.2.1v(reset usable, A few design innovations)

--- app/src/main/java/com/example/studyreports/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/studyreports/ResetPasswordActivity.kt
@@ -1,10 +1,11 @@
 package com.example.studyreports
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studyreports.databinding.ActivityResetPasswordBinding
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
 
 class ResetPasswordActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResetPasswordBinding
@@ -13,5 +14,23 @@ class ResetPasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityResetPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.resetButton.setOnClickListener {
+            val email = binding.resetEmailEditText.text.toString().trim()
+            if (email.isEmpty()) {
+                Toast.makeText(applicationContext, "Fields cannot be empty", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(applicationContext, "If there's an account associated with this email, we've sent a password reset link to it", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+        }
+        binding.resetCreateText.setOnClickListener{
+            startActivity(Intent(this,RegistrationActivity::class.java))
+        }
     }
 }
